c8162ee74e97886294fa587ece3c46199d04b2c7
Mayank Gupta <techfreakworm@gmail.com>
2019-07-24 01:21:46 +0530

    Create input and output component

---
+++ assignment-one/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const userOutput = () => {
+    return (
+        <div>
+            <p>Some random text</p>
+            <p>Some more random text!!!</p>
+        </div>
+    );
+}
+
+export default userOutput;
