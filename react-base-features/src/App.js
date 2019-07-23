861a593a2fcc51483eac22242f15cb0562a0e54e
Mayank Gupta <techfreakworm@gmail.com>
2019-07-23 19:58:57 +0530

    More understanding on JSX

--- react-base-features/src/App.js
+++ react-base-features/src/App.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    // return (
-    //     <div class="App">
-    //       <h1>Hi I'm a react app</h1>
-    //     </div>
-    //   );
-    return React.createElement('div', {className: 'App'}, '', React.createElement('h1', null, 'Does this work?'));
+    render() {
+        return (
+            <div class="App">
+              <h1>Hi I'm a react app</h1>
+              <p>This is really working</p>
+            </div>
+          );
+        // return React.createElement('div', { className: 'App' }, '', React.createElement('h1', null, 'Does this work?'));
     }
-  }
-  
-  export default App;
+}
+
+
+
+export default App;
