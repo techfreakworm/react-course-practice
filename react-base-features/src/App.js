c2a808a25d523db4e488ed3f1a0cf3ef89105ae5
Mayank Gupta <techfreakworm@gmail.com>
2019-07-23 20:23:43 +0530

    basic click event handling

--- react-base-features/src/App.js
+++ react-base-features/src/App.js
@@ -20,12 +20,16 @@ class App extends Component {
         ]
     }
 
+    switchNameHandler = () => {
+        console.log('Was clicked')
+    }
+
     render() {
         return (
-            <div class="App">
+            <div className="App">
               <h1>Hi I'm a react app</h1>
               <p>This is really working</p>
-              <button>Switch Name</button>
+              <button onClick={this.switchNameHandler}>Switch Name</button>
               <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
               <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
               <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
