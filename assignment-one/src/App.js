d9f0c79b27dd0e7f33642af7cae77057af6e34ed
Mayank Gupta <techfreakworm@gmail.com>
2019-07-24 01:29:53 +0530

    Use class state as property to be passed to component

--- assignment-one/src/App.js
+++ assignment-one/src/App.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput'
 
 
-function App() {
-  return (
-    <div>
-      <UserInput/>
-      <UserOutput userName="Max"/>
-      <UserOutput userName="Max"/>
-      <UserOutput userName="Max"/>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    userName: 'superMax'
+  }
+
+  render(){
+    return (
+      <div>
+        <UserInput/>
+        <UserOutput userName={this.state.userName}/>
+        <UserOutput userName={this.state.userName}/>
+        <UserOutput userName="Max"/>
+      </div>
+    );
+  }
 }
 
 export default App;
