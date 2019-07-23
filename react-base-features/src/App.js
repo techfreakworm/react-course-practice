cbeeeb0d6e2646a7cecf92d17bbc918adea04165
Mayank Gupta <techfreakworm@gmail.com>
2019-07-23 20:12:43 +0530

    Passing data dynamically to components

--- react-base-features/src/App.js
+++ react-base-features/src/App.js
@@ -8,10 +8,9 @@ class App extends Component {
             <div class="App">
               <h1>Hi I'm a react app</h1>
               <p>This is really working</p>
-              <Person />
-              <Person />
-              <Person />
-              <Person />
+              <Person name="Max" age="28"/>
+              <Person name="Manu" age="29">My Hobbies: Racing</Person>
+              <Person name="Stephanie" age="26"/>
             </div>
           );
         // return React.createElement('div', { className: 'App' }, '', React.createElement('h1', null, 'Does this work?'));
