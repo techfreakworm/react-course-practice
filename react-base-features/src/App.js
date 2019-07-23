1b5686afde8b9fecc3c8839fa3e830ff7933654c
Mayank Gupta <techfreakworm@gmail.com>
2019-07-23 20:04:40 +0530

    Create new component

--- react-base-features/src/App.js
+++ react-base-features/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
     render() {
@@ -7,6 +8,7 @@ class App extends Component {
             <div class="App">
               <h1>Hi I'm a react app</h1>
               <p>This is really working</p>
+              <Person />
             </div>
           );
         // return React.createElement('div', { className: 'App' }, '', React.createElement('h1', null, 'Does this work?'));
