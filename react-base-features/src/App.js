5a8b4bd7833605aa6fa4d9c7f513a5b5c4f04966
Mayank Gupta <techfreakworm@gmail.com>
2019-07-23 10:11:08 +0530

    Creating element using react methods instead of JSX code

--- react-base-features/src/App.js
+++ react-base-features/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-        <div class="App">
-          <h1>Hi I'm a react app</h1>
-        </div>
-      );
+    // return (
+    //     <div class="App">
+    //       <h1>Hi I'm a react app</h1>
+    //     </div>
+    //   );
+    return React.createElement('div', {className: 'App'}, '', React.createElement('h1', null, 'Does this work?'));
     }
   }
   
