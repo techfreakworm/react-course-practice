937d8b710075258a4d72e6ac6a94cc97d6c85321
Mayank Gupta <techfreakworm@gmail.com>
2019-07-23 20:20:07 +0530

    Using 'state' object to dynamically add data to components, //TODO: change state to re render the component

--- react-base-features/src/App.js
+++ react-base-features/src/App.js
@@ -3,14 +3,32 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+    state = {
+        persons: [
+            {
+                name: 'Max',
+                age: 28
+            },
+            {
+                name: 'Manu',
+                age: 29
+            },
+            {
+                name: 'Stephanie',
+                age: 26
+            }
+        ]
+    }
+
     render() {
         return (
             <div class="App">
               <h1>Hi I'm a react app</h1>
               <p>This is really working</p>
-              <Person name="Max" age="28"/>
-              <Person name="Manu" age="29">My Hobbies: Racing</Person>
-              <Person name="Stephanie" age="26"/>
+              <button>Switch Name</button>
+              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+              <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
             </div>
           );
         // return React.createElement('div', { className: 'App' }, '', React.createElement('h1', null, 'Does this work?'));
