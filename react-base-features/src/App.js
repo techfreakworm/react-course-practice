b7ad11b654160c0c0d62fadc8beb9434b34d996d
Mayank Gupta <techfreakworm@gmail.com>
2019-07-24 19:50:03 +0530

    Add radium to use pseudo css selectors

--- react-base-features/src/App.js
+++ react-base-features/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
+import Radium from "radium";
 
 class App extends Component {
   state = {
@@ -49,7 +50,11 @@ class App extends Component {
       font: "inherit",
       border: " 1px solid blue",
       padding: "8px",
-      cursor: "pointer"
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black"
+      }
     };
 
     let persons = null;
@@ -88,6 +93,10 @@ class App extends Component {
       );
 
       style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "salmon",
+        color: "black"
+      };
     }
 
     let classes = [];
@@ -114,4 +123,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
--- react-base-features/src/Person/Person.js
+++ react-base-features/src/Person/Person.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import './Person.css'
+import React from "react";
+import "./Person.css";
+import Radium from "radium";
 
-const person = (props) => {
-    return (
+const person = props => {
+  return (
     <div className="Person">
-        <p onClick={props.click}> I 'm {props.name} and I am {props.age} years old!</p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name}/>
+      <p onClick={props.click}>
+        {" "}
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
-    );
-}
+  );
+};
 
-export default person;
+export default Radium(person);
