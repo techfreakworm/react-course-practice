348ac17df0d0d1c4e3fee684b9f6b340523cec01
Mayank Gupta <techfreakworm@gmail.com>
2019-07-24 19:02:42 +0530

    Conditional JSX using conditions in JSX block

--- assignment-two/src/Validation/Validation.js
+++ assignment-two/src/Validation/Validation.js
@@ -5,7 +5,11 @@ const validation = (props) => {
 
     return (
         <div>
-            <p>Text too short!</p>
+            {
+                props.inputLength > 5 ?
+                    <p>Text long enough</p>:
+                    <p>Text too short!</p>
+            }
         </div>
     );
 };
