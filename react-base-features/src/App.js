ccabaa82f9c6fcc724a654765ab51c16be93cdf7
Mayank Gupta <techfreakworm@gmail.com>
2019-07-24 19:39:38 +0530

    Add linter, install prettier

--- react-base-features/src/App.js
+++ react-base-features/src/App.js
@@ -1,75 +1,74 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   state = {
     persons: [
-      { id: '1', name: 'Max', age: 28 },
-      { id: '12', name: 'Manu', age: 29 },
-      { id: '123', name: 'Stephanie', age: 26 }
+      { id: "1", name: "Max", age: 28 },
+      { id: "12", name: "Manu", age: 29 },
+      { id: "123", name: "Stephanie", age: 26 }
     ],
-    otherState: 'some other value',
+    otherState: "some other value",
     showPersons: false
   };
 
-  deletePersonHandler = (personIndex) => {
+  deletePersonHandler = personIndex => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons:persons});
+    this.setState({ persons: persons });
   };
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
-    })
+    });
 
-    
     // const person = Object.assign({}, this.state.persons[personIndex])
-    
-    const person = {...this.state.persons[personIndex]}
+
+    const person = { ...this.state.persons[personIndex] };
 
     person.name = event.target.value;
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({persons: persons});
+    this.setState({ persons: persons });
   };
 
   togglePersonHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow})
+    this.setState({ showPersons: !doesShow });
   };
 
   render() {
-      const style = {
-        backgroundColor: 'green',
-        color: 'white',
-        font: 'inherit',
-        border:' 1px solid blue',
-        padding: '8px',
-        cursor: 'pointer'
-      };
+    const style = {
+      backgroundColor: "green",
+      color: "white",
+      font: "inherit",
+      border: " 1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    };
 
-      let persons = null;
+    let persons = null;
 
-      if (this.state.showPersons) {
-        persons = (
-          <div >
-            {this.state.persons.map((person, index) => {
-              return (
-                <Person 
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
                 key={person.id}
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age}
-                changed={(event) => this.nameChangedHandler(event, person.id)}
-                />
-              );
-            })}
-            {/* <Person
+                changed={event => this.nameChangedHandler(event, person.id)}
+              />
+            );
+          })}
+          {/* <Person
               name={this.state.persons[0].name}
               age={this.state.persons[0].age}
             />
@@ -85,20 +84,20 @@ class App extends Component {
               name={this.state.persons[2].name}
               age={this.state.persons[2].age}
             /> */}
-          </div>
-        );
+        </div>
+      );
 
-        style.backgroundColor = 'red';
-      }
+      style.backgroundColor = "red";
+    }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button style={style}
-        onClick={this.togglePersonHandler}
-        >Toggle Persons</button>
-        {persons} 
+        <button style={style} onClick={this.togglePersonHandler}>
+          Toggle Persons
+        </button>
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
