e0eeb62e772c72b3ca413c4afc355c57276afa73
Mayank Gupta <techfreakworm@gmail.com>
2019-07-24 18:37:26 +0530

    Dynamically assigning new value to component (two way binding dynamically)

--- react-base-features/src/App.js
+++ react-base-features/src/App.js
@@ -5,9 +5,9 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: '1', name: 'Max', age: 28 },
+      { id: '12', name: 'Manu', age: 29 },
+      { id: '123', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
@@ -20,14 +20,22 @@ class App extends Component {
     this.setState({persons:persons});
   };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-        persons: [
-          { id:'123', name: "Max", age: 28 },
-          { id:'sdf3', name: event.target.value, age: 29 },
-          { id:'dfg3', name: 'Stephanie', age: 27 }
-        ]
-      });
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    })
+
+    
+    // const person = Object.assign({}, this.state.persons[personIndex])
+    
+    const person = {...this.state.persons[personIndex]}
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
   };
 
   togglePersonHandler = () => {
@@ -52,10 +60,11 @@ class App extends Component {
             {this.state.persons.map((person, index) => {
               return (
                 <Person 
-                key={person.name}
+                key={person.id}
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
                 />
               );
             })}
