8a5192108444a64fb0a8e843bdc82fe4473b847b
Mayank Gupta <techfreakworm@gmail.com>
2019-07-24 17:34:22 +0530

    Dynamically generate JSX by mapping to javascript array

--- react-base-features/src/App.js
+++ react-base-features/src/App.js
@@ -54,7 +54,15 @@ class App extends Component {
       if (this.state.showPersons) {
         persons = (
           <div >
-            <Person
+            {this.state.persons.map(person => {
+              return (
+                <Person 
+                name={person.name}
+                age={person.age}
+                />
+              );
+            })}
+            {/* <Person
               name={this.state.persons[0].name}
               age={this.state.persons[0].age}
             />
@@ -69,7 +77,7 @@ class App extends Component {
             <Person
               name={this.state.persons[2].name}
               age={this.state.persons[2].age}
-            />
+            /> */}
           </div>
         );
       }
