b897b8d73cd21795ed2dd06a226c17b360e6b4b9
Mayank Gupta <techfreakworm@gmail.com>
2019-07-24 17:39:06 +0530

    Javascript best practice: Make a copy object/array before modifying the source

--- react-base-features/src/App.js
+++ react-base-features/src/App.js
@@ -14,9 +14,10 @@ class App extends Component {
   };
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
-    persons.splice(personIndex, 1)
-    this.setState({persons: persons})
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons:persons});
   };
 
   nameChangedHandler = (event) => {
